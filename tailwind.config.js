46db9fc4b11cc0379ea1de11cf9ac8848f58b35a
joseook <pauloowneto@gmail.com>
2024-02-28 13:30:22 -0300

    feat added: completed

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+    },
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+    },
+  plugins: [],
+  }
+}
